962c0b9e3fe6d6602ff0e66b6e88feb889619b29
avina kefin <ojiappaijo@gmail.com>
2022-12-01 10:55:16 +0700

    Update env.sh
    add secret for custom anu

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@
 KERNEL_REPO=$KERNEL_SOURCE
 
 # Kernel Branch
-KERNEL_BRANCH=12.1-alioth
+KERNEL_BRANCH=main
 
 # The name of the device for which the kernel is built
 MODEL="Poco F3"
@@ -21,7 +21,7 @@ MANUFACTURERINFO="Xioami"
 # Kernel Variant
 NAMA=Samsoe
 
-JENIS=Local
+JENIS=MIUI-AOSP
 
 VARIAN=2.3.4
 
@@ -32,12 +32,12 @@ BUILD_TYPE="none"
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49' , 'linaro & 'gcc2', clang2
 COMPILER=clang
 
-# Message on anykernel when installatio
+# Message on anykernel when installation
 MESSAGE="just flash and forget"
 
 # KBUILD ENV
-K_USER=Avina
-K_HOST=Ubuntu
+K_USER=$BUILD_USER
+K_HOST=$HOST
 
 
 # arch & subarch
