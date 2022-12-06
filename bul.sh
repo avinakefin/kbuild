9c162fd49bc43d400cb1631c3688035afe21012e
avina kefin <ojiappaijo@gmail.com>
2022-12-07 06:02:31 +0700

    Update bul.sh

--- bul.sh
+++ bul.sh
@@ -72,7 +72,7 @@ FILES=Image.gz-dtb
 
 # Build dtbo.img (select this only if your source has support to building dtbo.img)
 # 1 is YES | 0 is NO(default)
-BUILD_DTBO=1
+BUILD_DTBO=0
 
 # Sign the zipfile
 # 1 is YES | 0 is NO
