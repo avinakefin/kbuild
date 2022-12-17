11b0f868b3a2494e1d03d71f436a987e9a7ee809
avina kefin <ojiappaijo@gmail.com>
2022-12-18 05:07:22 +0700

    Update bul.sh

--- bul.sh
+++ bul.sh
@@ -450,8 +450,7 @@ build_kernel() {
 
 gen_zip() {
 	msg "|| Zipping into a flashable zip ||"
-	mv "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb AnyKernel3/Image
-	mv "$KERNEL_DIR"/out/arch/arm64/boot/dtbo.img AnyKernel3/dtbo.img
+	mv "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb AnyKernel3/Image.gz-dtb
 
 	cd AnyKernel3 || exit
         cp -af anykernel-real.sh anykernel.sh
