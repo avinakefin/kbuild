4063bf7b946d56327b8bdc59a11e4fb77fa84353
avina kefin <ojiappaijo@gmail.com>
2023-02-07 23:27:04 +0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -4,13 +4,13 @@
 KERNEL_REPO=$KERNEL_SOURCE
 
 # Kernel Branch
-KERNEL_BRANCH=12.1-alioth
+KERNEL_BRANCH=munch_mi
 
 # The name of the device for which the kernel is built
-MODEL="Poco F3"
+MODEL="Poco F4"
 
 # The codename of the device
-DEVICE="alioth"
+DEVICE="munch"
 
 # The defconfig which should be used. Get it from config.gz from
 # your device or check source
@@ -21,16 +21,16 @@ MANUFACTURERINFO="Xioami"
 # Kernel Variant
 NAMA=Samsoe
 
-JENIS=MIUI-OOS-AOSP
+JENIS=MIUI
 
-VARIAN=2.3
+VARIAN=3.0
 
 # Build Type
 BUILD_TYPE="Stable"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49' , 'linaro & 'gcc2', clang2
-COMPILER=clang
+COMPILER=clang2
 
 # Message on anykernel when installation
 MESSAGE="just flash and forget"
