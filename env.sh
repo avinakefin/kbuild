bdfdf46737031856e3962dc43508089602b6ff7f
avina kefin <ojiappaijo@gmail.com>
2023-02-15 21:18:41 +0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -4,13 +4,13 @@
 KERNEL_REPO=$KERNEL_SOURCE
 
 # Kernel Branch
-KERNEL_BRANCH=staging
+KERNEL_BRANCH=12.1-alioth
 
 # The name of the device for which the kernel is built
-MODEL="Poco F4"
+MODEL="Poco F3"
 
 # The codename of the device
-DEVICE="munch"
+DEVICE="alioth"
 
 # The defconfig which should be used. Get it from config.gz from
 # your device or check source
@@ -21,16 +21,16 @@ MANUFACTURERINFO="Xioami"
 # Kernel Variant
 NAMA=Yaknah
 
-JENIS=ASOP
+JENIS=MIUI
 
-VARIAN=2.3
+VARIAN=Limited
 
 # Build Type
 BUILD_TYPE="Stable"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49' , 'linaro & 'gcc2', clang2
-COMPILER=aosp
+COMPILER=clang
 
 # Message on anykernel when installation
 MESSAGE="just flash and forget"
