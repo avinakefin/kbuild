dc0ca242d037f002692ce271a632fab1262528d6
avina kefin <ojiappaijo@gmail.com>
2023-02-18 17:13:57 +0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -30,7 +30,7 @@ BUILD_TYPE="Stable"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc (zynz)' or 'gcc' or 'gcc49' , 'linaro & 'gcc2', clang2
-COMPILER=clang
+COMPILER=clang2
 
 # Message on anykernel when installation
 MESSAGE="just flash"
