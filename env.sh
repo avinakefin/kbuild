4cf0e27df012dc90dadbe93ea205538ce56a2d7e
avina kefin <ojiappaijo@gmail.com>
2023-07-02 00:11:17 +0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -7,7 +7,7 @@ KERNEL_REPO=$KERNEL_SOURCE
 KERNEL_BRANCH=base
 
 # The name of the device for which the kernel is built
-MODEL="Poco F3"
+MODEL="Poco F4"
 
 # The codename of the device
 DEVICE=$DEVICE_CODENAME
@@ -19,12 +19,12 @@ DEVICE=$DEVICE_CODENAME
 MANUFACTURERINFO="Xioami"
 
 # Kernel Variant
-NAMA=Samsoe
+NAMA=Yaknah
 
 # untuk skalian fik ini itu pada miui , tulis kecil semua [ miui ]
 JENIS=aosp
 
-VARIAN=2.9
+VARIAN=Rev
 
 # Build Type
 BUILD_TYPE="Stable"
@@ -32,7 +32,7 @@ BUILD_TYPE="Stable"
 #
 # Specify compiler.
 # 'clang' or 'clangxgcc (zynz)' or 'gcc' or 'gcc49' , 'linaro & 'gcc2', clang2
-COMPILER=clang
+COMPILER=aosp
 
 # Message on anykernel when installation
 MESSAGE="just flash and forget"
