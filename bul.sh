6ce5ae26db79755f25dc6303fcb5c9636662067b
avina kefin <ojiappaijo@gmail.com>
2023-07-10 06:42:56 +0700

    Update bul.sh

--- bul.sh
+++ bul.sh
@@ -228,10 +228,8 @@ exports() {
 	elif [ $COMPILER = "clangxgcc" ]
 	then
 		KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-		PATH=$TC_DIR/bin/:$PATH
-                export TARGET_SOC=s5e9925
-                export LLVM=1 LLVM_IAS=1
-		export PATH=$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH
+		PATH=$TC_DIR/bin/:$PATH          
+		PATH=$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH
 	elif [ $COMPILER = "gcc49" ]
 	then
 		KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-linux-android-gcc --version | head -n 1 )
