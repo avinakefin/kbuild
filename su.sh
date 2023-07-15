a11d511ea966a0c6191157d644bef3fd20d1201a
avina kefin <ojiappaijo@gmail.com>
2023-07-15 22:53:16 +0700

    Update su.sh

--- su.sh
+++ su.sh
@@ -18,7 +18,9 @@ fi
 test -d "$GKI_ROOT/KernelSU" || git clone https://github.com/tiann/KernelSU
 cd "$GKI_ROOT"
 
-echo "[+] GKI_ROOT: $GKI_ROOT"
+echo "[+] buat folder kernelsu"
+mkdir drivers/kernelsu
+
 echo "[+] Copy kernel su driver to $DRIVER_DIR"
 
 cd "$DRIVER_DIR"
@@ -29,7 +31,7 @@ elif test -d "$GKI_ROOT/drivers"; then
 fi
 cd "$GKI_ROOT"
 echo "copy file to driver"
-     cp $GKI_ROOT/KernelSU/kernel $GKI_ROOT/drivers
+     cp -rf $GKI_ROOT/KernelSU/kernel $GKI_ROOT/drivers
 
 echo '[+] Add kernel su driver to Makefile'
 
@@ -37,5 +39,7 @@ DRIVER_MAKEFILE=$DRIVER_DIR/Makefile
 DRIVER_KCONFIG=$DRIVER_DIR/Kconfig
 grep -q "kernelsu" "$DRIVER_MAKEFILE" || printf "obj-\$(CONFIG_KSU) += kernelsu/\n" >> "$DRIVER_MAKEFILE"
 grep -q "kernelsu" "$DRIVER_KCONFIG" || sed -i "/endmenu/i\\source \"drivers/kernelsu/Kconfig\"" "$DRIVER_KCONFIG"
+grep -q "drivers/kernel" "$DRIVER_MAKEFILE" || printf "obj-\$(CONFIG_KSU) += kernel/\n" >> "$DRIVER_MAKEFILE"
+grep -q "drivers/kernel" "$DRIVER_KCONFIG" || sed -i "/endmenu/i\\source \"drivers/kernel/Kconfig\"" "$DRIVER_KCONFIG"
 
 echo '[+] Done.'
