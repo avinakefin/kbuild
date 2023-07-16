a594b237cf3bed8d333d325e1bc094898e0bf942
avina kefin <ojiappaijo@gmail.com>
2023-07-16 20:58:53 +0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -4,10 +4,10 @@
 KERNEL_REPO=$KERNEL_SOURCE
 
 # Kernel Branch
-KERNEL_BRANCH=KERNEL.PLATFORM.1.0.r1-15300-kernel.0
+KERNEL_BRANCH=alioth
 
 # The name of the device for which the kernel is built
-MODEL="Poco F5"
+MODEL="Poco F3"
 
 # The codename of the device
 DEVICE=$DEVICE_CODENAME
@@ -19,7 +19,7 @@ DEVICE=$DEVICE_CODENAME
 MANUFACTURERINFO="Xioami"
 
 # Kernel Variant
-NAMA=Yaknah_F5
+NAMA=Samsoe
 
 # untuk skalian fik ini itu pada miui , tulis kecil semua [ miui ]
 JENIS=aosp
@@ -27,7 +27,7 @@ JENIS=aosp
 # 1 if clone anykernel 0 for have anykernel
 ANYKERNEL=1
 
-VARIAN=1.0-anu
+VARIAN=3.0
 
 # Build Type
 BUILD_TYPE="Stable"
@@ -35,7 +35,7 @@ BUILD_TYPE="Stable"
 #
 # Specify compiler.
 # 'clang' or 'clangxgcc (zynz)' or 'zyn (clang)' or 'gcc49' , 'linaro for kernel 5.4 'gcc2', clang2
-COMPILER=linaro
+COMPILER=clang
 
 # Message on anykernel when installation
 MESSAGE="just flash and forget"
@@ -46,7 +46,7 @@ K_HOST=$HOST
 
 # arch & subarch
 K_ARCH=arm64
-#K_SUBARCH=arm64
+K_SUBARCH=arm64
 
 # Clone kernelSU 1 yes or 0 no
 CLONESU=1
