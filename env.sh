853c90221982ec5f6c29c4fe417174ed065d31e4
avina kefin <ojiappaijo@gmail.com>
2023-08-10 18:01:58 +0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@
 KERNEL_REPO=$KERNEL_SOURCE
 
 # Kernel Branch
-KERNEL_BRANCH=alioth
+KERNEL_BRANCH=main
 
 # The name of the device for which the kernel is built
 MODEL="Poco F3"
@@ -19,15 +19,15 @@ DEVICE=$DEVICE_CODENAME
 MANUFACTURERINFO="Xioami"
 
 # Kernel Variant
-NAMA=Samsoe
+NAMA=Yaknah
 
 # untuk skalian fik ini itu pada miui , tulis kecil semua [ miui ]
 JENIS=Stable
 
-VARIAN=3.1-KcalDefault
+VARIAN=1
 
 # Build Type
-BUILD_TYPE="Stable"
+BUILD_TYPE="Evo"
 
 #
 # Specify compiler.
