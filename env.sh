2991eace886ad9b4c43aa20b5f1326b8e170a327
avina kefin <ojiappaijo@gmail.com>
2023-08-15 03:39:49 +0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -35,7 +35,7 @@ BUILD_TYPE="Last_2.0"
 #
 # Specify compiler.
 # 'clang' or 'clangxgcc (zynz)' or 'zyn (clang)' or 'gcc49' , 'linaro for kernel 5.4 'gcc2', clang2
-COMPILER=clang
+COMPILER=linaro
 
 # Message on anykernel when installation
 MESSAGE="just flash and forget"
