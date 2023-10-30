ff74152a7b09d8986dee87aca8100ae7d0c4bcec
avina kefin <ojiappaijo@gmail.com>
2023-10-30 09:30:24 +0700

    Update bul.sh

--- bul.sh
+++ bul.sh
@@ -584,9 +584,9 @@ gen_zip() {
 	     fi
         fi
     
-        if [ -f "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb ]
+        if [ -f "$KERNEL_DIR"/out/arch/arm64/boot/Image ]
         then
-            mv "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb AnyKernel3/Image.gz-dtb
+            mv "$KERNEL_DIR"/out/arch/arm64/boot/Image AnyKernel3/Image
     
         else [ -f "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz ]
         
