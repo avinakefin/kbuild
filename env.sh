0ef5c0bf43778661228cd38d42a385e6cc28a825
avina kefin <ojiappaijo@gmail.com>
2024-01-02 17:50:32 +0700

    Use MIUI clang

--- env.sh
+++ env.sh
@@ -32,7 +32,7 @@ BUILD_TYPE="KSU"
 #
 # Specify compiler.
 # 'clang' or 'clangxgcc (zynz)' or 'zyn (clang)' or 'gcc49' , 'linaro for kernel 5.4 'gcc2', clang2
-COMPILER=clang
+COMPILER=miui
 
 # Message on anykernel when installation
 MESSAGE="just flash and forget"
