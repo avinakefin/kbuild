c8ab438565e7ae291455ee39f6df86ebfe23c5b6
avina kefin <ojiappaijo@gmail.com>
2024-01-16 17:44:44 +0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@
 KERNEL_REPO=$KERNEL_SOURCE
 
 # Kernel Branch
-KERNEL_BRANCH=t
+KERNEL_BRANCH=alioth
 
 # The name of the device for which the kernel is built
 MODEL="Poco F3"
@@ -24,7 +24,7 @@ NAMA=samsoe
 # untuk skalian fik ini itu pada miui , tulis kecil semua [ miui ]
 JENIS=aosp
 
-VARIAN=t-samsoe
+VARIAN=f3
 
 # Build Type
 BUILD_TYPE="OOS AOSP HyoerOS"
